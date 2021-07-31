700c0fa4853fb2b736a9947c3f9d75081a87ded1
HarryWangATX <harrynicomama@gmail.com>
2021-07-31 12:40:36 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'next/core-web-vitals',
   ],
   rules: {
-    'prettier/prettier': 'warning',
+    'prettier/prettier': 'warn',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
