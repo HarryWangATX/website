a59c6b4deb2661578d2cb5b60faf225ad50e6de9
HarryWangATX <harrynicomama@gmail.com>
2022-07-06 18:49:55 -0500

    added pdf viewing component

---
+++ components/PDFViewer.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from "react-pdf";
+
+// import 'css/pdf.less'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const options = {
+    cMapUrl: 'cmaps/',
+    cMapPacked: true,
+    standardFontDataUrl: 'standard_fonts/',
+};
+
+const PDFViewer = ({filePath}) => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
+
+    return (
+        <div>
+            <Document
+                file={filePath}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={pageNumber} />
+
+                <div className="page-controls">
+                    <button
+                        disabled={pageNumber <= 1}
+                        onClick={() => setPageNumber(pageNumber - 1)}
+                        type="button"
+                        aria-label="Previous page"
+                    >
+                        ‹
+                    </button>
+                    <span>
+                  {pageNumber}
+                        {' '}
+                        of
+                        {' '}
+                        {numPages}
+                </span>
+                    <button
+                        disabled={pageNumber >= numPages}
+                        onClick={() => setPageNumber(pageNumber + 1)}
+                        type="button"
+                        aria-label="Next page"
+                    >
+                        ›
+                    </button>
+                </div>
+            </Document>
+        </div>
+
+);
+}
+
+export default PDFViewer
+
+/*
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+import './Sample.css';
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+  standardFontDataUrl: 'standard_fonts/',
+};
+
+export default function Sample() {
+  const [file, setFile] = useState('./sample.pdf');
+  const [numPages, setNumPages] = useState(null);
+
+  function onFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>{' '}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document">
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
+ */
