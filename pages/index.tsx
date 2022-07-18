1011bcc66d615b8eae0dcebb331c961db5083bb2
HarryWangATX <harrynicomama@gmail.com>
2022-07-18 18:41:10 -0500

    fix dup subscription

--- pages/index.tsx
+++ pages/index.tsx
@@ -257,11 +257,11 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
-          <div className="flex items-center justify-center pt-4">
-              <NewsletterForm />
-          </div>
-      )}
+      {/*{siteMetadata.newsletter.provider !== '' && (*/}
+      {/*    <div className="flex items-center justify-center pt-4">*/}
+      {/*        <NewsletterForm />*/}
+      {/*    </div>*/}
+      {/*)}*/}
     </>
   )
 }
